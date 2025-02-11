97a59684e0d1a6b5cdce0439ed619693fc4e5605
Sophia See <sophiasee11@gmail.com>
2025-02-11 23:09:03 +0800

    updated navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,14 +5,42 @@ import { SearchInput } from "./custom-ui/search-input";
 import Image from 'next/image'
 import { CiSettings } from "react-icons/ci";
 import React from "react";
-import { redirect } from "next/navigation";
+import { redirect, usePathname } from "next/navigation";
+
+const LINK_NAMES = [
+    {
+        name: "All Notes",
+        path: "/home"
+    },
+    {
+        name: "Archived Notes",
+        path: "/archived"
+    },
+    {
+        name: "Notes Tagged: ",
+        path: "/tags"
+    },
+    {
+        name: "Showing results for: ",
+        path: "/search"
+    },
+    {
+        name: "Settings",
+        path: "/settings"
+    },
+]
 
 export default function Navbar() {
     const { isDarkMode } = useAppContext();
+    const pathname = usePathname();
+
+    const renderTitle = React.useMemo(() => {
+        return LINK_NAMES.find(i => pathname.includes(i.path))?.name ?? "";
+    }, [pathname])
 
     const renderLogo = React.useMemo(() => {
         return (
-            <div className='w-[95px] h-[28px]'>
+            <div className='w-[95px] h-[28px] lg:hidden'>
                 <Image
                     src={`/assets/images/logo${isDarkMode ? "Dark" : ""}.svg`}
                     width={0}
@@ -42,7 +70,7 @@ export default function Navbar() {
         >
             {renderLogo}
             <div className={`hidden lg:block text-preset-1 text-accent-foreground`}>
-                All Notes
+                {renderTitle}
             </div>
             <div className="hidden lg:flex gap-4 items-center">
                 <SearchInput 
